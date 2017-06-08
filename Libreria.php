dfe8829859fcf2296c3bc4f34af30d8cea578f11
jmronco <>
2017-06-08 12:43:34 -0400

    Uso JQueryv2

---
+++ Libreria.php
@@ -0,0 +1,10 @@
+<?php
+include 'Constantes.php';
+include PATH.'/Lib/Conexion.php';
+include PATH.'/Lib/Usuario.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
